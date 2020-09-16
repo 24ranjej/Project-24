cc697803a645337c6e1febda848be078281aebe8
24ranjej <69919771+24ranjej@users.noreply.github.com>
2020-09-16 06:59:42 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var dustbinB,dustbinL,dustbinR;
+var ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1400, 700);
+
+	ground = createSprite(700,680,1400,20);
+	ground.shapeColor = color("brown");
+
+	
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
